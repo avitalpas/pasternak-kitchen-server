30a26c22bcbe6439044d827dd669503cf3ce7ea6
avitalpas <65176812+avitalpas@users.noreply.github.com>
2024-02-05 00:56:51 +0200

    .

--- routes/recipe.route.js
+++ routes/recipe.route.js
@@ -15,4 +15,52 @@ router.route('/:id').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
+// post new recipe
+router.route('/new').post((req, res) => {
+
+    const newRecipe = new Recipe({ 
+        Title: req.body.Title,
+        Chef: req.body.Chef,
+        Quantity: req.body.Quantity,
+        Ingredients: req.body.Ingredients,
+        Description: req.body.Description,
+        Source: req.body.Source,
+        Image: req.body.Image,
+        Tag: req.body.Tag,
+        Healthy: req.body.Healthy
+     })
+
+     newRecipe.save()
+        .then(() => res.json('Recipe added!'))
+        .catch(err => res.status(400).json('Error: + err'))
+})
+
+// edit recipe by id
+router.route('/edit/:id').post((req, res) => {
+    Recipe.findById(req.params.id)
+        .then(recipe => {
+            recipe.Title = req.body.Title
+            recipe.Chef = req.body.Chef
+            recipe.Quantity = req.body.Quantity
+            recipe.Ingredients = req.body.Ingredients
+            recipe.Description = req.body.Description
+            recipe.Source = req.body.Source
+            recipe.Image = req.body.Image
+            recipe.Tag = req.body.Tag
+            recipe.Healthy = req.body.Healthy
+
+            recipe.save()
+                .then(() => res.json('Recipe updated!'))
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+// delete recipe by id
+router.route('/:id').delete((req, res) => {
+    Recipe.findByIdAndDelete(req.params.id)
+        .then( () => res.json('recipe deleted'))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
 module.exports = router
